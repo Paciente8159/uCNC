23c186334d7cee5bc227fa48211fa89d96b9ac7c
Paciente8159 <Paciente8159@users.noreply.github.com>
2022-02-04 22:07:01 +0000

    Update cnc_config.h
    -reset default config file

--- uCNC/cnc_config.h
+++ uCNC/cnc_config.h
@@ -201,7 +201,7 @@ extern "C"
  * Performs motions with variable acceleration (trapezoidal speed profile with roundend speed transition between accel/deaccel and constant speed)
  * instead of constant acceleration (trapezoizal speed profile)
  * */
-#define ENABLE_S_CURVE_ACCELERATION
+// #define ENABLE_S_CURVE_ACCELERATION
 
 /**
 	 * Forces pin pooling for all limits and control pins (with or without interrupts)
