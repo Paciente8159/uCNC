e820bd20871f74a173573ea71388b1c1f43be81a
Paciente8159 <Paciente8159@users.noreply.github.com>
2022-02-27 21:30:32 +0000

    Update parser.c
    -force parser_last_pos reset on init to prevent NAND values on target if initial G command does not have all coordinates.

--- uCNC/src/core/parser.c
+++ uCNC/src/core/parser.c
@@ -140,8 +140,8 @@ void parser_init(void)
 #ifdef FORCE_GLOBALS_TO_0
     memset(&parser_state, 0, sizeof(parser_state_t));
     memset(&parser_parameters, 0, sizeof(parser_parameters_t));
-    memset(parser_last_pos, 0, sizeof(parser_last_pos));
 #endif
+    memset(parser_last_pos, 0, sizeof(parser_last_pos));
     parser_parameters_load();
     parser_reset();
 }
