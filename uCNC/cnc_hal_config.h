97b17e3c09db81414d816525dd1ed1fb820bf4ed
Paciente8159 <Paciente8159@users.noreply.github.com>
2022-03-30 15:04:56 +0100

    default settings

--- uCNC/cnc_hal_config.h
+++ uCNC/cnc_hal_config.h
@@ -147,7 +147,7 @@ extern "C"
 #define TMC_SPI 2
 
 // uncomment to enable trinamic driver
-#define STEPPER0_HAS_TMC
+//#define STEPPER0_HAS_TMC
 #ifdef STEPPER0_HAS_TMC
 #define STEPPER0_DRIVER_TYPE 2208
 // choose the interface type
@@ -171,7 +171,7 @@ extern "C"
 #define STEPPER0_ENABLE_INTERPLATION true
 #endif
 // uncomment to enable trinamic driver
-#define STEPPER1_HAS_TMC
+//#define STEPPER1_HAS_TMC
 #ifdef STEPPER1_HAS_TMC
 #define STEPPER1_DRIVER_TYPE 2208
 // choose the interface type
