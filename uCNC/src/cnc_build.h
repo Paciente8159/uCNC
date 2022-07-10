74e782eb8a7966a2637553852c319e917d4b521a
Paciente8159 <Paciente8159@users.noreply.github.com>
2022-07-10 11:05:14 +0100

    Version bump

--- uCNC/src/cnc_build.h
+++ uCNC/src/cnc_build.h
@@ -25,7 +25,7 @@ extern "C"
 #endif
 
 #define CNC_MAJOR_MINOR_VERSION "1.4"
-#define CNC_PATCH_VERSION ".6"
+#define CNC_PATCH_VERSION ".7pre"
 
 #define CNC_VERSION CNC_MAJOR_MINOR_VERSION CNC_PATCH_VERSION
 
