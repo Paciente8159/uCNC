6e84fd2788c0efcb6b497a07e8b316a20100593b
Paciente8159 <Paciente8159@users.noreply.github.com>
2022-10-31 13:09:18 +0000

    generate file

---
+++ uCNC/src/hal/boards/board_user_overrides.h
@@ -0,0 +1,15 @@
+//board override dummy file
+
+#ifndef BOARD_USER_OVERRIDES_H
+#define BOARD_USER_OVERRIDES_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
