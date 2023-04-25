9c1861c3a00cdb586f258ca962bea0b8861c7d63
Paciente8159 <Paciente8159@users.noreply.github.com>
2023-04-26 00:18:16 +0100

    resolved warnings and errors
    - resolved warnings and errors related to menu declarations

--- uCNC/src/modules/system_menu.c
+++ uCNC/src/modules/system_menu.c
@@ -1,19 +1,19 @@
 /*
-    Name: system_menu.c
-    Description: System menus for displays for µCNC.
+	Name: system_menu.c
+	Description: System menus for displays for µCNC.
 
-    Copyright: Copyright (c) João Martins
-    Author: João Martins
-    Date: 20-04-2023
+	Copyright: Copyright (c) João Martins
+	Author: João Martins
+	Date: 20-04-2023
 
-    µCNC is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version. Please see <http://www.gnu.org/licenses/>
+	µCNC is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version. Please see <http://www.gnu.org/licenses/>
 
-    µCNC is distributed WITHOUT ANY WARRANTY;
-    Also without the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-    See the	GNU General Public License for more details.
+	µCNC is distributed WITHOUT ANY WARRANTY;
+	Also without the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+	See the	GNU General Public License for more details.
 */
 
 #include "system_menu.h"
@@ -29,193 +29,181 @@ DECL_MENU(2, 1, "Settings menu", 0, NULL);
 
 system_menu_t g_system_menu;
 
-void system_menu_init(void)
+static const system_menu_item_t *system_menu_get_item(uint8_t menu_id, int16_t index)
 {
-    g_system_menu.current_menu = 0;
-    g_system_menu.current_index = 0;
-    g_system_menu.menu_entry = NULL;
-    g_system_menu.flags = SYSTEM_MENU_RENDER;
-    system_menu_append(MENU(1));
-    system_menu_append(MENU(2));
+	MENU_LOOP(g_system_menu.menu_entry, menu_item)
+	{
+		if (menu_item->menu_id == menu_id)
+		{
+			// in the item group
+			if (index < menu_item->item_count)
+			{
+				return menu_item->items[index];
+			}
+			else
+			{
+				// must be in one extended menu
+				index -= menu_item->item_count;
+				// get the next set of extended menu entries
+			}
+		}
+	}
 
-#ifdef ENABLE_MAIN_LOOP_MODULES
-    ADD_EVENT_LISTENER(cnc_dotasks, system_menu_render);
-    ADD_EVENT_LISTENER(cnc_reset, system_menu_render);
-#endif
-}
-
-static system_menu_item_t *system_menu_get_item(uint8_t menu_id, uint8_t index)
-{
-    MENU_LOOP(g_system_menu.menu_entry, menu_item)
-    {
-        if (menu_item->menu_id == menu_id)
-        {
-            // in the item group
-            if (index < menu_item->item_count)
-            {
-                return menu_item->items[index];
-            }
-            else
-            {
-                // must be in one extended menu
-                index - menu_item->item_count;
-                // get the next set of extended menu entries
-            }
-        }
-    }
-
-    // could not find
-    // return empty item
-    return NULL;
+	// could not find
+	// return empty item
+	return NULL;
 }
 
 void system_menu_append(system_menu_page_t *extended_menu)
 {
-    system_menu_page_t *ptr = g_system_menu.menu_entry;
+	system_menu_page_t *ptr = g_system_menu.menu_entry;
 
-    while (ptr->extended != NULL)
-    {
-        ptr = ptr->extended;
-    }
+	while (ptr->extended != NULL)
+	{
+		ptr = ptr->extended;
+	}
 
-    ptr->extended = extended_menu;
+	ptr->extended = extended_menu;
 }
 
 void system_menu_reset(void)
 {
-    g_system_menu.current_menu = 0;
-    g_system_menu.current_index = 0;
-    g_system_menu.flags = SYSTEM_MENU_STARTUP | SYSTEM_MENU_RENDER;
-    g_system_menu.idle_timeout = mcu_millis() + SYSTEM_MENU_IDLE_TIMEOUT_MS;
+	g_system_menu.current_menu = 0;
+	g_system_menu.current_index = 0;
+	g_system_menu.flags = SYSTEM_MENU_STARTUP | SYSTEM_MENU_RENDER;
+	g_system_menu.next_redraw = mcu_millis() + SYSTEM_MENU_IDLE_TIMEOUT_MS;
 }
 
+CREATE_EVENT_LISTENER(cnc_dotasks, system_menu_render);
 void system_menu_render(void)
 {
-    uint8_t renderflags = g_system_menu.flags;
-    g_system_menu.flags &= ~SYSTEM_MENU_RENDER;
+	uint8_t renderflags = g_system_menu.flags;
+	g_system_menu.flags &= ~SYSTEM_MENU_RENDER;
 
-    if(g_system_menu.idle_timeout < mcu_millis())
+	if (g_system_menu.next_redraw < mcu_millis())
+	{
+		// the render flag is active
+		if (renderflags & SYSTEM_MENU_RENDER)
+		{
+			if (renderflags & SYSTEM_MENU_ALARM)
+			{
+				system_menu_render_startup();
+				return;
+			}
 
-    // the render flag is active
-    if (renderflags & SYSTEM_MENU_RENDER)
-    {
-        if (renderflags & SYSTEM_MENU_ALARM)
-        {
-            system_menu_render_startup();
-            return;
-        }
+			if (renderflags & SYSTEM_MENU_STARTUP)
+			{
+				system_menu_render_startup();
+				return;
+			}
 
-        if (renderflags & SYSTEM_MENU_STARTUP)
-        {
-            system_menu_render_startup();
-            return;
-        }
+			if (renderflags & SYSTEM_MENU_IDLE)
+			{
+				system_menu_render_idle();
+				return;
+			}
 
-        if (renderflags & SYSTEM_MENU_IDLE)
-        {
-            system_menu_render_idle();
-            return;
-        }
+			if (!g_system_menu.current_menu)
+			{
+				// nothing to render
+				return;
+			}
+			else
+			{
+				uint8_t item_index = 0;
+				MENU_LOOP(g_system_menu.menu_entry, menu_item)
+				{
+					if (menu_item->menu_id == g_system_menu.current_menu)
+					{
+						if (!item_index)
+						{
+							system_menu_render_header(menu_item->label);
+						}
 
-        if (!g_system_menu.current_menu)
-        {
-            // nothing to render
-            return;
-        }
-        else
-        {
-            MENU_LOOP(g_system_menu.menu_entry, menu_item)
-            {
-                if (menu_item->menu_id == g_system_menu.current_menu)
-                {
-                    system_menu_render_header(menu_item->label);
-                    break;
-                }
-            }
+						for (uint8_t i = 0; i < menu_item->item_count; i++)
+						{
+							if (menu_item->items[i]->render)
+							{
+								menu_item->items[i]->render(menu_item->items[i]->render_arg);
+							}
+							else
+							{
+								system_menu_render_content(item_index + i, menu_item->items[i]);
+							}
+						}
+						item_index += menu_item->item_count;
+					}
+				}
+			}
+		}
 
-            MENU_LOOP(g_system_menu.menu_entry, menu_item)
-            {
-                if (menu_item->menu_id == g_system_menu.current_menu)
-                {
-                    uint8_t item_index = 0;
-                    for (uint8_t i = 0; i < menu_item->item_count; i++)
-                    {
-                        if (menu_item->items[i]->render)
-                        {
-                            menu_item->items[i]->render(menu_item->items[i]->render_arg);
-                        }
-                        else
-                        {
-                            system_menu_render_content(item_index + i, menu_item->label);
-                        }
-                    }
-                    item_index += menu_item->item_count;
-                }
-            }
-        }
-    }
+		g_system_menu.next_redraw = mcu_millis() + SYSTEM_MENU_REDRAW_MS;
+	}
 }
 
 void system_menu_action(uint8_t action)
 {
-    static uint32_t last_action_time = 0;
-    uint32_t current_time = mcu_millis();
+	static uint32_t last_action_time = 0;
+	uint32_t current_time = mcu_millis();
 
-    bool is_idle = ((current_time - last_action_time) > SYSTEM_MENU_IDLE_TIMEOUT_MS);
+	bool is_idle = ((current_time - last_action_time) > SYSTEM_MENU_IDLE_TIMEOUT_MS);
 
-    if (action)
-    {
-        is_idle = false;
-        last_action_time = current_time;
-    }
-    else if (is_idle)
-    {
-        return;
-    }
+	if (action)
+	{
+		is_idle = false;
+		last_action_time = current_time;
+	}
+	else if (is_idle)
+	{
+		g_system_menu.flags = SYSTEM_MENU_IDLE;
+		return;
+	}
 
-    switch (action)
-    {
-    case SYSTEM_MENU_ACTION_SELECT:
-        if (!g_system_menu.current_menu)
-        {
-            // enter main menu
-            g_system_menu.current_menu = 1;
-            g_system_menu.current_index = 0;
-        }
-        else
-        {
-            // if inside a menu get get the arg
-            system_menu_item_t *next = system_menu_get_item(g_system_menu.current_menu, g_system_menu.current_index);
-            if (next)
-            {
-                // found custom action and execute
-                if (next->action)
-                {
-                    next->action(next->action_arg);
-                    return;
-                }
-            }
-            else
-            {
-                // something went wrong (menu not found)
-                // return to home screen
-                g_system_menu.current_menu = 0;
-                g_system_menu.current_index = 0;
-            }
-        }
-        break;
-    case SYSTEM_MENU_ACTION_NEXT:
-        g_system_menu.current_index++;
-        break;
-    case SYSTEM_MENU_ACTION_PREV:
-        g_system_menu.current_index--;
-        break;
-    default:
-        // no new action don't render
-        return;
-    }
+	switch (action)
+	{
+	case SYSTEM_MENU_ACTION_SELECT:
+		if (!g_system_menu.current_menu)
+		{
+			// enter main menu
+			g_system_menu.current_menu = 1;
+			g_system_menu.current_index = 0;
+		}
+		else
+		{
+			// if inside a menu get the arg
+			const system_menu_item_t *next = system_menu_get_item(g_system_menu.current_menu, g_system_menu.current_index);
+			if (next)
+			{
+				system_menu_item_t item = {0};
+				rom_memcpy(&item, next, sizeof(system_menu_item_t));
+				// found custom action and execute
+				if (item.action)
+				{
+					item.action(item.action_arg);
+					return;
+				}
+			}
+			else
+			{
+				// something went wrong (menu not found)
+				// return to home screen
+				g_system_menu.current_menu = 0;
+				g_system_menu.current_index = 0;
+			}
+		}
+		break;
+	case SYSTEM_MENU_ACTION_NEXT:
+		g_system_menu.current_index++;
+		break;
+	case SYSTEM_MENU_ACTION_PREV:
+		g_system_menu.current_index--;
+		break;
+	default:
+		// no new action don't render
+		return;
+	}
 
-    g_system_menu.flags |= SYSTEM_MENU_RENDER;
+	g_system_menu.flags |= SYSTEM_MENU_RENDER;
 }
 
 /**
@@ -225,47 +213,60 @@ void system_menu_action(uint8_t action)
 // calls a new menu
 void system_menu_action_goto(void *cmd)
 {
-    g_system_menu.current_menu = (uint8_t)cmd;
-    g_system_menu.current_index = 0;
-    g_system_menu.flags |= SYSTEM_MENU_RENDER;
+	g_system_menu.current_menu = (uint8_t)cmd;
+	g_system_menu.current_index = 0;
+	g_system_menu.flags |= SYSTEM_MENU_RENDER;
 }
 
 void system_menu_action_rt_cmd(void *cmd)
 {
-    cnc_call_rt_command((uint8_t)cmd);
+	cnc_call_rt_command((uint8_t)cmd);
 }
 
 void system_menu_action_serial_cmd(void *cmd)
 {
-    serial_inject_cmd((const char *)cmd, false);
+	serial_inject_cmd((const char *)cmd, false);
 }
 
 void __attribute__((weak)) system_menu_render_header(const char *__s)
 {
-    protocol_send_string(__romstr__("*----------*"));
-    protocol_send_string(MSG_EOL);
-    protocol_send_string(__s);
-    protocol_send_string(MSG_EOL);
-    protocol_send_string(__romstr__("*----------*"));
-    protocol_send_string(MSG_EOL);
+	// render the menu header
 }
 
-void __attribute__((weak)) system_menu_render_content(uint8_t index, const char *__s)
+void __attribute__((weak)) system_menu_render_content(uint8_t item_index, const system_menu_item_t *item)
 {
-    protocol_send_string(__s);
-    protocol_send_string(MSG_EOL);
+	// render item
 }
 
 void __attribute__((weak)) system_menu_render_startup(void)
 {
+	// render startup screen
 }
 
 void __attribute__((weak)) system_menu_render_idle(void)
 {
+	// render idle screen
+	// this is usually the screen showing the position and status of the machine
 }
 
 void __attribute__((weak)) system_menu_render_alarm(void)
 {
+	// render alarm screen
+}
+
+void system_menu_init(void)
+{
+	g_system_menu.current_menu = 0;
+	g_system_menu.current_index = 0;
+	g_system_menu.menu_entry = NULL;
+	g_system_menu.flags = SYSTEM_MENU_RENDER;
+	system_menu_append(MENU(1));
+	system_menu_append(MENU(2));
+
+#ifdef ENABLE_MAIN_LOOP_MODULES
+	ADD_EVENT_LISTENER(cnc_dotasks, system_menu_render);
+	// ADD_EVENT_LISTENER(cnc_reset, system_menu_render);
+#endif
 }
 
 #endif
--- uCNC/src/modules/system_menu.h
+++ uCNC/src/modules/system_menu.h
@@ -1,19 +1,19 @@
 /*
-    Name: system_menu.h
-    Description: System menus for displays for µCNC.
+	Name: system_menu.h
+	Description: System menus for displays for µCNC.
 
-    Copyright: Copyright (c) João Martins
-    Author: João Martins
-    Date: 20-04-2023
+	Copyright: Copyright (c) João Martins
+	Author: João Martins
+	Date: 20-04-2023
 
-    µCNC is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version. Please see <http://www.gnu.org/licenses/>
+	µCNC is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version. Please see <http://www.gnu.org/licenses/>
 
-    µCNC is distributed WITHOUT ANY WARRANTY;
-    Also without the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-    See the	GNU General Public License for more details.
+	µCNC is distributed WITHOUT ANY WARRANTY;
+	Also without the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+	See the	GNU General Public License for more details.
 */
 
 #ifndef SYSTEM_MENU_H
@@ -36,12 +36,16 @@ extern "C"
 #define SYSTEM_MENU_IDLE_TIMEOUT_MS 5000
 #endif
 
+#ifndef SYSTEM_MENU_REDRAW_MS
+#define SYSTEM_MENU_REDRAW_MS 200
+#endif
+
 // render flags
 // the higher the bit the higher the priority
 #define SYSTEM_MENU_ALARM 128
 #define SYSTEM_MENU_STARTUP 64
-#define SYSTEM_MENU_IDLE 8
 #define SYSTEM_MENU_RENDER 1
+#define SYSTEM_MENU_IDLE 0
 
 #define SYSTEM_MENU_ACTION_NONE 0
 #define SYSTEM_MENU_ACTION_SELECT 1
@@ -50,34 +54,34 @@ extern "C"
 
 #define CONST_VARG(X) ((void *)X)
 
-    typedef struct system_menu_entry_
-    {
-        char menu_name[SYSTEM_MENU_MAX_STR_LEN];
-        void *argptr;
-        void (*render)(void *);
-        void *render_arg;
-        void (*action)(void *);
-        void *action_arg;
-    } system_menu_item_t;
+	typedef struct system_menu_entry_
+	{
+		char label[SYSTEM_MENU_MAX_STR_LEN];
+		void *argptr;
+		void (*render)(void *);
+		void *render_arg;
+		void (*action)(void *);
+		void *action_arg;
+	} system_menu_item_t;
 
-    typedef struct system_menu_page_
-    {
-        uint8_t menu_id;
-        uint8_t parent_id;
-        const char *label;
-        uint8_t item_count;
-        system_menu_item_t **items;
-        struct system_menu_page_ *extended;
-    } system_menu_page_t;
+	typedef struct system_menu_page_
+	{
+		uint8_t menu_id;
+		uint8_t parent_id;
+		const char *label;
+		uint8_t item_count;
+		system_menu_item_t** const items;
+		struct system_menu_page_ *extended;
+	} system_menu_page_t;
 
-    typedef struct system_menu_
-    {
-        uint8_t flags;
-        uint8_t current_menu;
-        uint8_t current_index;
-        system_menu_page_t *menu_entry;
-        uint32_t idle_timeout;
-    } system_menu_t;
+	typedef struct system_menu_
+	{
+		uint8_t flags;
+		uint8_t current_menu;
+		uint8_t current_index;
+		system_menu_page_t *menu_entry;
+		uint32_t next_redraw;
+	} system_menu_t;
 
 #define MENU_ENTRY(name) ((system_menu_item_t *)&name)
 #define DECL_MENU_ENTRY(name, strvalue, argptr, display_cb, display_cb_arg, action_cb, action_cb_arg) static const system_menu_item_t name __rom__ = {strvalue, argptr, display_cb, display_cb_arg, action_cb, action_cb_arg}
@@ -89,44 +93,47 @@ extern "C"
 #define DECL_MENU_GOTO(name, strvalue, menu) DECL_MENU_ENTRY(name, strvalue, NULL, NULL, NULL, system_menu_action_goto, menu)
 #define DECL_MENU_ACTION(name, strvalue, action_cb, action_cb_arg) DECL_MENU_ENTRY(name, strvalue, NULL, NULL, NULL, action_cb, action_cb_arg)
 
-#define DECL_MENU(id, parent_id, label, count, ...) static system_menu_page_t m##id = {id, parent_id, label, count, (system_menu_item_t **){__VA_ARGS__}, NULL}
+#define DECL_MENU(id, parent_id, label, count, ...)  \
+	static const char m##id##_label[] __rom__ = label; \
+	static const system_menu_item_t* m##id##_items[] = {__VA_ARGS__}; \
+	static system_menu_page_t m##id = {id, parent_id, m##id##_label, count, m##id##_items, NULL}
 #define MENU(id) (&m##id)
 
 #define MENU_LOOP(page, item) for (system_menu_page_t *item = page; item != NULL; item = item->extended)
 
 #ifdef ENABLE_SYSTEM_MENU
-    extern system_menu_t g_system_menu;
+	extern system_menu_t g_system_menu;
 #endif
 
 	DECL_MODULE(system_menu);
-    void system_menu_append(system_menu_page_t *extended_menu);
-    void system_menu_render(void);
-    void system_menu_reset(void);
-    void system_menu_action(uint8_t action);
+	void system_menu_append(system_menu_page_t *extended_menu);
+	void system_menu_render(void);
+	void system_menu_reset(void);
+	void system_menu_action(uint8_t action);
 
-    /**
-     * Overridable functions to be implemented for the display to render the system menu
-     * **/
-    void system_menu_render_header(const char *__s);
-    void system_menu_render_content(uint8_t index, const char *__s);
-    void system_menu_render_footer(system_menu_page_t *menu);
-    void system_menu_render_startup(void);
-    void system_menu_render_idle(void);
-    void system_menu_render_alarm(void);
+	/**
+	 * Overridable functions to be implemented for the display to render the system menu
+	 * **/
+	void system_menu_render_header(const char *__s);
+	void system_menu_render_content(uint8_t item_index, const system_menu_item_t *item);
+	void system_menu_render_footer(system_menu_page_t *menu);
+	void system_menu_render_startup(void);
+	void system_menu_render_idle(void);
+	void system_menu_render_alarm(void);
 
-    /**
-     * Helper µCNC action callbacks
-     * **/
-    void system_menu_action_goto(void *cmd);
-    void system_menu_action_rt_cmd(void *cmd);
-    void system_menu_action_serial_cmd(void *cmd);
+	/**
+	 * Helper µCNC action callbacks
+	 * **/
+	void system_menu_action_goto(void *cmd);
+	void system_menu_action_rt_cmd(void *cmd);
+	void system_menu_action_serial_cmd(void *cmd);
 
-    /**
-     * Helper µCNC render callbacks
-     * **/
-    void system_menu_label_var(void *cmd);
-    void system_menu_action_rt_cmd(void *cmd);
-    void system_menu_action_serial_cmd(void *cmd);
+	/**
+	 * Helper µCNC render callbacks
+	 * **/
+	void system_menu_label_var(void *cmd);
+	void system_menu_action_rt_cmd(void *cmd);
+	void system_menu_action_serial_cmd(void *cmd);
 
 #ifdef __cplusplus
 }
