6b63a040bbfb2f82f972ab8c36c07cb93d8de800
Paciente8159 <Paciente8159@users.noreply.github.com>
2023-05-04 11:15:41 +0100

    LPC and STM32 pinouts for display adapter

--- uCNC/src/hal/boards/lpc176x/boardmap_re_arm.h
+++ uCNC/src/hal/boards/lpc176x/boardmap_re_arm.h
@@ -159,16 +159,28 @@ extern "C"
 // #define DIN31_BIT 0
 // #define DIN31_PORT 0
 
-// hardware SPI
-#define SPI_SDO_BIT 9
+// hardware SPI (onboard)
+// #define SPI_SDO_BIT 9
+// #define SPI_SDO_PORT 0
+// #define SPI_SDI_BIT 8
+// #define SPI_SDI_PORT 0
+// #define SPI_CLK_BIT 7
+// #define SPI_CLK_PORT 0
+// #define SPI_CS_BIT 6
+// #define SPI_CS_PORT 0
+// #define SPI_PORT 1
+
+// hardware SPI (display adapter)
+#define SPI_SDO_BIT 18
 #define SPI_SDO_PORT 0
-#define SPI_SDI_BIT 8
+#define SPI_SDI_BIT 17
 #define SPI_SDI_PORT 0
-#define SPI_CLK_BIT 7
+#define SPI_CLK_BIT 15
 #define SPI_CLK_PORT 0
-#define SPI_CS_BIT 6
+#define SPI_CS_BIT 23
 #define SPI_CS_PORT 0
-#define SPI_PORT 1
+
+// #define SPI_PORT 1
 // #define SPI_FREQ 100000UL
 // software SPI
 // #define DOUT29_BIT 9
@@ -177,18 +189,11 @@ extern "C"
 // #define DIN29_PORT 0
 // #define DOUT30_BIT 7
 // #define DOUT30_PORT 0
-// sd card detect
-// #define DIN19_BIT 0
-// #define DIN19_PORT L
-// #define DIN19_PULLUP
 
-// mapping for reprap full discount display
-#define DOUT8_BIT 15
-#define DOUT8_PORT 0
-#define DOUT9_BIT 18
-#define DOUT9_PORT 0
-#define DOUT10_BIT 16
-#define DOUT10_PORT 0
+// sd card detect
+#define DIN19_BIT 31
+#define DIN19_PORT 1
+#define DIN19_PULLUP
 
 //pins for smart adapter
 //clk
