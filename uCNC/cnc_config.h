4e97d0ae420468dd249ee35cfc938626b5f96982
Paciente8159 <Paciente8159@users.noreply.github.com>
2024-03-03 21:20:08 +0000

    fixed interpolator accelerations calculations
    - fixed interpolator accelerations calculations by preventing time slices that are too small leading to super slow accelerations and deaccelerations

--- uCNC/cnc_config.h
+++ uCNC/cnc_config.h
@@ -506,7 +506,7 @@ extern "C"
 	 * helps to reduce code size if features are not needed
 	 * */
 #ifndef DISABLE_ALL_CONTROLS
-// #define DISABLE_ALL_CONTROLS
+#define DISABLE_ALL_CONTROLS
 #endif
 #ifndef DISABLE_ALL_LIMITS
 // #define DISABLE_ALL_LIMITS
