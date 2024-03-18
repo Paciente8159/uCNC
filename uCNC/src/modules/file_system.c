dcc6adeffe9a9b909125087d4e473aff731df9eb
Paciente8159 <Paciente8159@users.noreply.github.com>
2024-03-18 18:01:42 +0000

    partial integration of system menu

--- uCNC/src/modules/file_system.c
+++ uCNC/src/modules/file_system.c
@@ -19,32 +19,26 @@
 #include "../cnc.h"
 #include "file_system.h"
 #include "endpoint.h"
+#include "system_menu.h"
 
-#ifndef FS_STR_DIR_PREFIX
-#define FS_STR_DIR_PREFIX "Directory "
-#endif
-#ifndef FS_STR_FILE_NOT_FOUND
-#define FS_STR_FILE_NOT_FOUND " not found!\n"
-#endif
-#ifndef FS_STR_FILE_NOT_DIR
-#define FS_STR_FILE_NOT_DIR " is not a dir!\n"
-#endif
-#ifndef FS_FILE_READ_ERROR
-#define FS_FILE_READ_ERROR "File read error!"
-#endif
-#ifndef FS_FILE_READ_END
-#define FS_FILE_READ_END "File read finished"
-#endif
-#ifndef FS_FILE_RUN
-#define FS_FILE_RUN "Running file from line "
-#endif
 #ifndef FS_MAX_PATH_LEN
 #define FS_MAX_PATH_LEN 128
 #endif
 
+// file system entry point
 fs_t *fs_default_drive;
-// current working dir
+// current working dir for internal file system (Grbl commands)
 static char fs_cwd[FS_MAX_PATH_LEN];
+#ifdef MCU_HAS_ENDPOINTS
+// current working dir for endpoints
+static char fs_endpoint_cwd[FS_MAX_PATH_LEN];
+#endif
+// current working dir for system menu (graphic display)
+static char fs_sm_cwd[FS_MAX_PATH_LEN];
+// dir level for system menu (graphic display)
+static uint8_t dir_level;
+
+// current running file
 fs_file_t *fs_running_file;
 
 static fs_t *fs_search_drive(char *path)
@@ -90,13 +84,13 @@ static fs_t *fs_search_drive(char *path)
 	return NULL;
 }
 
-static char *fs_parentdir(void)
+static char *fs_parentdir(char *path)
 {
-	char *tail = strrchr(fs_cwd, '/');
+	char *tail = strrchr(path, '/');
 	if (!tail)
 	{
 		// is in root
-		tail = fs_cwd;
+		tail = path;
 	}
 
 	*tail = 0;
@@ -105,17 +99,54 @@ static char *fs_parentdir(void)
 }
 
 // emulates basic chdir and opens the dir or file if it exists
-static fs_file_t *fs_chfile(const char *newdir, char *mode)
+static fs_file_t *fs_path_parse(char *path, const char *newdir, char *mode)
 {
+	// new algorithm to test
+	//  if(newdir){
+	//  	if(newdir[0]=='/'){
+	//  		path = newdir;
+	//  	}
+	//  }
+	//  char *token;
+	//    char *pathComponents[MAX_PATH_LENGTH];
+	//    int pathCount = 0;
+
+	//   // Tokenize the path using "/"
+	//   token = strtok(path, "/");
+	//   while (token != NULL && pathCount < MAX_PATH_LENGTH) {
+	//       // Check if the token is "." or ".."
+	//       if (strcmp(token, ".") == 0) {
+	//           // Do nothing, it's a reference to the current directory
+	//       } else if (strcmp(token, "..") == 0) {
+	//           // Move one level up
+	//           if (pathCount > 0)
+	//               pathCount--;
+	//       } else {
+	//           // Add regular directory name to the path
+	//           pathComponents[pathCount++] = token;
+	//       }
+	//       token = strtok(NULL, "/");
+	//   }
+
+	//   // Reconstruct the path
+	//   int i, j = 0;
+	//   for (i = 0; i < pathCount; i++) {
+	//       int len = strlen(pathComponents[i]);
+	//       memmove(path + j, pathComponents[i], len);
+	//       j += len;
+	//       path[j++] = '/';
+	//   }
+	//   path[j - 1] = '\0'; // Remove the last '/'
+
 	int16_t state = 0;
-	uint16_t len = strlen(fs_cwd);
-	char *tail = &fs_cwd[len];
+	uint16_t len = strlen(path);
+	char *tail = &path[len];
 	fs_file_t *fp = NULL;
 
 	if (*newdir == '/')
 	{
 		// root dir
-		tail = fs_cwd;
+		tail = path;
 		len = 0;
 		*tail = 0;
 		newdir++;
@@ -137,27 +168,27 @@ static fs_file_t *fs_chfile(const char *newdir, char *mode)
 			{
 			case 2:
 				*tail = 0;
-				tail = fs_parentdir();
+				tail = fs_parentdir(path);
 				// continue
 			case 1:
 				// deletes dot or slash
 				*tail = 0;
-				tail = fs_parentdir();
+				tail = fs_parentdir(path);
 				*tail = *newdir;
-				len = strlen(fs_cwd);
+				len = strlen(path);
 				break;
 			default:
 				if (state && state < FS_MAX_PATH_LEN)
 				{
 					// path with only dots not allowed
 					*tail = 0;
-					tail = fs_parentdir();
+					tail = fs_parentdir(path);
 					return NULL;
 				}
 			}
 
 			*tail = 0;
-			fp = fs_open(fs_cwd, mode);
+			fp = fs_open(path, mode);
 			// checks if is valid dir
 			if (fp)
 			{
@@ -177,7 +208,7 @@ static fs_file_t *fs_chfile(const char *newdir, char *mode)
 					if (!fp->file_info.is_dir)
 					{
 						// rewind
-						tail = fs_parentdir();
+						tail = fs_parentdir(path);
 					}
 
 					return fp;
@@ -204,7 +235,7 @@ static fs_file_t *fs_chfile(const char *newdir, char *mode)
 		if (len >= FS_MAX_PATH_LEN)
 		{
 			// clamp
-			tail = strrchr(fs_cwd, '/');
+			tail = strrchr(path, '/');
 			*tail = '/';
 			tail++;
 			*tail = 0;
@@ -220,7 +251,7 @@ static fs_file_t *fs_chfile(const char *newdir, char *mode)
 static void fs_dir_list(void)
 {
 	// current dir
-	protocol_send_string(__romstr__(FS_STR_DIR_PREFIX));
+	protocol_send_string(__romstr__("Index of "));
 	serial_print_str(fs_cwd);
 	protocol_send_string(MSG_EOL);
 
@@ -228,26 +259,24 @@ static void fs_dir_list(void)
 	dir = fs_open(fs_cwd, "r");
 	if (!dir)
 	{
-		protocol_send_string(__romstr__(FS_STR_FILE_NOT_FOUND));
+		protocol_send_string(__romstr__("Dir not found!"));
 		return;
 	}
 
-	if (!dir->is_dir)
+	if (!dir->file_info.is_dir)
 	{
-		protocol_send_string(__romstr__(FS_STR_FILE_NOT_DIR));
+		protocol_send_string(__romstr__("Is not a valid dir!"));
 	}
 	else
 	{
 		while (true)
 		{
-			char filename[32];
-			memset(filename, 0, 32);
-			fs_file_t *nextfile = dir->next_file(filename);
-			if (!nextfile)
+			fs_file_info_t finfo;
+			if (!dir->fs_ptr->next_file(dir, &finfo))
 			{
 				break;
 			}
-			if (nextfile->is_dir)
+			if (finfo.is_dir)
 			{ /* It is a directory */
 				protocol_send_string(__romstr__("<dir>\t"));
 			}
@@ -256,10 +285,10 @@ static void fs_dir_list(void)
 				protocol_send_string(__romstr__("     \t"));
 			}
 
-			uint8_t i = strlen(filename);
+			uint8_t i = strlen(finfo.name);
 			for (uint8_t j = 0; j < i; j++)
 			{
-				serial_putc(filename);
+				serial_putc(finfo.name);
 			}
 			protocol_send_string(MSG_EOL);
 		}
@@ -285,7 +314,7 @@ void fs_cd(void)
 
 	newdir[i] = 0;
 
-	fs_file_t *fp = fs_chfile(newdir, "r");
+	fs_file_t *fp = fs_path_parse(fs_cwd, newdir, "r");
 	if (fp)
 	{
 		if (strlen(fs_cwd))
@@ -302,7 +331,7 @@ void fs_cd(void)
 	else
 	{
 		serial_print_str(newdir);
-		protocol_send_feedback(__romstr__(FS_STR_FILE_NOT_FOUND));
+		protocol_send_feedback(__romstr__("File not found!"));
 	}
 
 	protocol_send_string(MSG_EOL);
@@ -325,16 +354,16 @@ void fs_file_print(void)
 
 	file[i] = 0;
 
-	fs_file_t *fp = fs_chfile(file, "r");
+	fs_file_t *fp = fs_path_parse(fs_cwd, file, "r");
 	if (fp)
 	{
-		while (fp->available())
+		while (fs_available(fp))
 		{
 			memset(file, 0, RX_BUFFER_CAPACITY);
-			i = (uint8_t)fp->read(file, RX_BUFFER_CAPACITY - 1); /* Read the data */
+			i = (uint8_t)fs_read(fp, file, RX_BUFFER_CAPACITY - 1); /* Read the data */
 			if (!i)
 			{
-				protocol_send_feedback(__romstr__(FS_FILE_READ_ERROR));
+				protocol_send_feedback(__romstr__("File read error!"));
 				break;
 			}
 			file[i] = 0;
@@ -342,48 +371,49 @@ void fs_file_print(void)
 		}
 
 		fs_close(fp);
-		protocol_send_feedback(__romstr__(FS_FILE_READ_END));
+		protocol_send_feedback(__romstr__("File ended"));
 		return;
 	}
 	else
 	{
-		protocol_send_feedback(__romstr__(FS_STR_FILE_NOT_FOUND));
+		protocol_send_feedback(__romstr__("File not found!"));
 	}
 
 	protocol_send_string(MSG_EOL);
 }
 
-static uint8_t fs_getc(void)
+static uint8_t running_file_getc(void)
 {
 	uint8_t c = 0;
 	if (fs_running_file)
 	{
-		int avail = fs_running_file->available();
+		int avail = fs_available(fs_running_file);
 		if (avail)
 		{
-			fs_running_file->read(&c, 1);
+			fs_read(fs_running_file, &c, 1);
 			// auto close file
 			if (--avail)
 			{
 				fs_close(fs_running_file);
+				fs_running_file = NULL;
 			}
 		}
 	}
 	return 0;
 }
 
-static uint8_t fs_available()
+static uint8_t running_file_available()
 {
 	uint8_t avail = 0;
 	if (fs_running_file)
 	{
-		uint8_t avail = (uint8_t)MIN(255, fs_running_file->available());
+		uint8_t avail = (uint8_t)MIN(255, fs_available(fs_running_file));
 	}
 
 	return avail;
 }
 
-static void fs_clear()
+static void running_file_clear()
 {
 	if (fs_running_file)
 	{
@@ -420,19 +450,19 @@ void fs_file_run(void)
 		file++;
 	}
 
-	fs_file_t *fp = fs_chfile(file, "r");
+	fs_file_t *fp = fs_path_parse(fs_cwd, file, "r");
 
 	if (fp)
 	{
 		protocol_send_string(MSG_START);
-		protocol_send_string(__romstr__(FS_FILE_RUN " - "));
+		protocol_send_string(__romstr__("Running file from line - "));
 		serial_print_int(startline);
 		protocol_send_string(MSG_END);
 #ifdef DECL_SERIAL_STREAM
 		// open a readonly stream
 		// the output is sent to the current holding interface
 		fs_running_file = fp;
-		serial_stream_readonly(&fs_getc, &fs_available, &fs_clear);
+		serial_stream_readonly(&running_file_getc, &running_file_available, &running_file_clear);
 		while (startline)
 		{
 			parser_discard_command();
@@ -506,7 +536,7 @@ void fs_json_api(void)
 		while (ptr)
 		{
 			memset(path, 0, sizeof(path));
-			sprintf(path, "{\"type\":\"drive\",\"name\":\"%c\"},", ptr->drive);
+			snprintf(path, 32, "{\"type\":\"drive\",\"name\":\"%c\"},", ptr->drive);
 			ptr = ptr->next;
 			if (ptr)
 			{
@@ -577,7 +607,7 @@ void fs_file_json_api()
 		{
 			endpoint_send_header("Location", args, true);
 			memset(urlpath, 0, sizeof(urlpath));
-			sprintf(urlpath, "{\"redirect\":\"%s\"}", args);
+			snprintf(urlpath, 256, "{\"redirect\":\"%s\"}", args);
 			endpoint_send(303, "application/json", urlpath);
 		}
 		else
@@ -601,11 +631,11 @@ void fs_file_json_api()
 				memset(urlpath, 0, 256);
 				if (child.is_dir)
 				{
-					sprintf(urlpath, "{\"type\":\"dir\",\"name\":\"%s\",\"attr\":%d},", child.name, 0);
+					snprintf(urlpath, 256, "{\"type\":\"dir\",\"name\":\"%s\",\"attr\":%d},", child.name, 0);
 				}
 				else
 				{
-					sprintf(urlpath, "{\"type\":\"file\",\"name\":\"%s\",\"attr\":0,\"size\":%d,\"date\":0}", child.name, child.size);
+					snprintf(urlpath, 256, "{\"type\":\"file\",\"name\":\"%s\",\"attr\":0,\"size\":%d,\"date\":0}", child.name, child.size);
 				}
 
 				if (fs_nextfile(file, &child))
@@ -621,15 +651,15 @@ void fs_file_json_api()
 		}
 		else
 		{
-			uint8_t content[ENDPOINT_MAX_CHUNCK_LEN];
+			char content[ENDPOINT_MAX_CHUNCK_LEN / sizeof(char)];
 			if (file->file_info.size > ENDPOINT_MAX_CHUNCK_LEN)
 			{
 				endpoint_send(200, "application/octet-stream", NULL);
 			}
 			while (fs_available(file))
 			{
-				fs_read(file, content, ENDPOINT_MAX_CHUNCK_LEN);
-				endpoint_send(200, "application/octet-stream", (const char *)content);
+				fs_read(file, (uint8_t *)content, ENDPOINT_MAX_CHUNCK_LEN);
+				endpoint_send(200, "application/octet-stream", content);
 			}
 			// close the stream
 			endpoint_send(200, "application/octet-stream", "");
@@ -645,6 +675,226 @@ void fs_json_uploader()
 }
 
 #endif
+
+static void system_menu_render_fs_item(uint8_t render_flags, system_menu_item_t *item)
+{
+	char buffer[SYSTEM_MENU_MAX_STR_LEN];
+
+	if (!fs_default_drive)
+	{
+		rom_strcpy(buffer, __romstr__(FS_STR_UNMOUNTED));
+	}
+	else if (fs_running_file)
+	{
+		rom_strcpy(buffer, __romstr__(FS_STR_FILE_RUNNING));
+	}
+	else
+	{
+		rom_strcpy(buffer, __romstr__(FS_STR_MOUNTED));
+	}
+
+	system_menu_item_render_arg(render_flags, buffer);
+}
+
+static bool system_menu_action_fs_item(uint8_t action, system_menu_item_t *item)
+{
+	if (action == SYSTEM_MENU_ACTION_SELECT)
+	{
+		if (fs_running_file)
+		{
+			// currently running job
+			// do nothing
+		}
+		else
+		{
+			// go back to root dir
+			sd_chfile("/", 0);
+			dir_level = 0;
+			// goto sd card menu
+			g_system_menu.current_menu = 10;
+			g_system_menu.current_index = 0;
+			g_system_menu.current_multiplier = 0;
+			g_system_menu.flags &= ~(SYSTEM_MENU_MODE_EDIT | SYSTEM_MENU_MODE_MODIFY);
+		}
+
+		return true;
+	}
+
+	return false;
+}
+
+// dynamic rendering of the sd card menu
+// lists all dirs and files
+static void system_menu_sd_card_render(uint8_t render_flags)
+{
+	uint8_t cur_index = g_system_menu.current_index;
+
+	if (render_flags & SYSTEM_MENU_MODE_EDIT)
+	{
+		system_menu_render_header(current_file.fname);
+		char buffer[SYSTEM_MENU_MAX_STR_LEN];
+		memset(buffer, 0, SYSTEM_MENU_MAX_STR_LEN);
+		rom_strcpy(buffer, __romstr__(SD_STR_FILE_PREFIX SD_STR_SD_CONFIRM));
+		system_menu_item_render_label(render_flags, buffer);
+		system_menu_item_render_arg(render_flags, current_file.fname);
+	}
+	else
+	{
+		FRESULT res;
+		FILINFO fno;
+		DIR dp;
+
+		// current dir
+		if (!strlen(cwd))
+		{
+			system_menu_render_header("/\0");
+		}
+		else
+		{
+			char *last_slash = strrchr(cwd, '/');
+			if (last_slash == NULL)
+			{
+				last_slash = cwd;
+			}
+			else
+			{
+				last_slash++;
+			}
+			system_menu_render_header(last_slash);
+		}
+		uint8_t index = 0;
+		if (sd_opendir(&dp, cwd) == FR_OK)
+		{
+			for (;;)
+			{
+				res = sd_readdir(&dp, &fno); /* Read a directory item */
+				if (res != FR_OK || fno.fname[0] == 0)
+				{
+					break; /* Break on error or end of dir */
+				}
+
+				if (system_menu_render_menu_item_filter(index))
+				{
+					char buffer[SYSTEM_MENU_MAX_STR_LEN];
+					memset(buffer, 0, SYSTEM_MENU_MAX_STR_LEN);
+					buffer[0] = (fno.fattrib & AM_DIR) ? '/' : ' ';
+					memcpy(&buffer[1], fno.fname, MIN(SYSTEM_MENU_MAX_STR_LEN - 1, strlen(fno.fname)));
+					system_menu_item_render_label(render_flags | ((cur_index == index) ? SYSTEM_MENU_MODE_SELECT : 0), buffer);
+					// stores the current file info
+					if ((cur_index == index))
+					{
+						memcpy(&current_file, &fno, sizeof(FILINFO));
+					}
+				}
+				index++;
+			}
+			g_system_menu.total_items = index;
+			sd_closedir(&dp);
+		}
+	}
+
+	system_menu_render_nav_back((g_system_menu.current_index < 0 || g_system_menu.current_multiplier < 0));
+	system_menu_render_footer();
+}
+
+bool system_menu_sd_card_action(uint8_t action)
+{
+	uint8_t render_flags = g_system_menu.flags;
+	bool go_back = (g_system_menu.current_index < 0 || g_system_menu.current_multiplier < 0);
+
+	// selects a file or a dir
+	if (action == SYSTEM_MENU_ACTION_SELECT)
+	{
+		char buffer[SYSTEM_MENU_MAX_STR_LEN];
+
+		if (render_flags & SYSTEM_MENU_MODE_EDIT)
+		{
+			// file print or quit
+			// if it's over the nav back element
+			if (go_back)
+			{
+				// don't run file and return to render sd content
+				g_system_menu.flags &= ~SYSTEM_MENU_MODE_EDIT;
+			}
+			else
+			{
+				// run file
+				if (sd_chfile(current_file.fname, FA_READ) == FR_OK)
+				{
+					file_runs = 1;
+					protocol_send_string(MSG_START);
+					protocol_send_string(__romstr__(SD_STR_FILE_PREFIX SD_STR_SD_RUNNING " - "));
+					serial_print_int(file_runs);
+					protocol_send_string(MSG_END);
+					system_menu_go_idle();
+					rom_strcpy(buffer, __romstr__(SD_STR_FILE_PREFIX SD_STR_SD_RUNNING));
+					system_menu_show_modal_popup(SYSTEM_MENU_MODAL_POPUP_MS, buffer);
+				}
+				else
+				{
+					rom_strcpy(buffer, __romstr__(SD_STR_FILE_PREFIX SD_STR_SD_FAILED));
+					system_menu_show_modal_popup(SYSTEM_MENU_MODAL_POPUP_MS, buffer);
+					sd_fclose();
+				}
+			}
+		}
+		else
+		{
+			if (go_back)
+			{
+				if (dir_level)
+				{
+					// up one dirs
+					sd_chfile("..", 0);
+					g_system_menu.current_index = 0;
+					g_system_menu.current_multiplier = 0;
+					g_system_menu.total_items = 0;
+					dir_level--;
+					return true;
+				}
+				else
+				{
+					// return back let system menu handle it
+					return false;
+				}
+			}
+
+			if (current_file.fname[0] != 0)
+			{
+				if ((current_file.fattrib & AM_DIR))
+				{
+					if (sd_chfile(current_file.fname, 0) == FR_OK)
+					{
+						g_system_menu.current_index = 0;
+						g_system_menu.current_multiplier = 0;
+						g_system_menu.total_items = 0;
+						dir_level++;
+					}
+					else
+					{
+						rom_strcpy(buffer, __romstr__(SD_STR_SD_PREFIX SD_STR_SD_ERROR));
+						system_menu_show_modal_popup(SYSTEM_MENU_MODAL_POPUP_MS, buffer);
+					}
+				}
+				else
+				{
+					// go to file run or quit menu
+					g_system_menu.current_multiplier = 0;
+					g_system_menu.flags |= SYSTEM_MENU_MODE_EDIT;
+				}
+			}
+			else
+			{
+				rom_strcpy(buffer, __romstr__(SD_STR_SD_PREFIX SD_STR_SD_ERROR));
+				system_menu_show_modal_popup(SYSTEM_MENU_MODAL_POPUP_MS, buffer);
+			}
+		}
+
+		return true;
+	}
+
+	return false;
+}
 
 void fs_mount(fs_t *drive)
 {
--- uCNC/src/modules/file_system.h
+++ uCNC/src/modules/file_system.h
@@ -40,38 +40,56 @@ extern "C"
 		bool is_dir;
 		uint32_t size;
 		uint32_t timestamp;
-	}fs_file_info_t;
+	} fs_file_info_t;
 
 	typedef struct fs_file_
 	{
 		fs_file_info_t file_info;
-		struct fs_* fs_ptr;
-		void* file_ptr;
+		struct fs_ *fs_ptr;
+		void *file_ptr;
 	} fs_file_t;
 
 	typedef struct fs_
 	{
 		char drive;
 		fs_file_t *(*open)(char *, const char *);
-		size_t (*read)(fs_file_t*, uint8_t *, size_t);
-		size_t (*write)(fs_file_t*, const uint8_t *, size_t);
-		int (*available)(fs_file_t*);
-		void (*close)(fs_file_t*);
+		size_t (*read)(fs_file_t *, uint8_t *, size_t);
+		size_t (*write)(fs_file_t *, const uint8_t *, size_t);
+		int (*available)(fs_file_t *);
+		void (*close)(fs_file_t *);
 		bool (*remove)(char *);
-		bool (*next_file)(fs_file_t *, fs_file_info_t*);
+		bool (*next_file)(fs_file_t *, fs_file_info_t *);
 		bool (*finfo)(char *, fs_file_info_t *);
-		struct fs_* next;
+		struct fs_ *next;
 	} fs_t;
 
 	void fs_mount(fs_t *drive);
 	void fs_unmount(fs_t *drive);
-	fs_file_t* fs_open(char *path, const char *mode);
-	size_t fs_read(fs_file_t* fp, uint8_t * buffer, size_t len);
-	size_t fs_write(fs_file_t* fp, const uint8_t * buffer, size_t len);
-	int fs_available(fs_file_t* fp);
-	void fs_close(fs_file_t* fp);
+	fs_file_t *fs_open(char *path, const char *mode);
+	size_t fs_read(fs_file_t *fp, uint8_t *buffer, size_t len);
+	size_t fs_write(fs_file_t *fp, const uint8_t *buffer, size_t len);
+	int fs_available(fs_file_t *fp);
+	void fs_close(fs_file_t *fp);
 	bool fs_remove(char *path);
 	bool fs_nextfile(fs_file_t *fp, fs_file_info_t *finfo);
+
+// All non translatable strings
+// #define FS_STR_DIR_PREFIX "Directory "
+// #define FS_STR_FILE_NOT_FOUND " not found!\n"
+// #define FS_STR_FILE_NOT_DIR " is not a dir!\n"
+// #define FS_FILE_READ_ERROR "File read error!"
+// #define FS_FILE_READ_END "File read finished"
+// #define FS_FILE_RUN "Running file from line "
+// All translatable strings
+#ifndef FS_STR_UNMOUNTED
+#define FS_STR_UNMOUNTED "FS unmounted"
+#endif
+#ifndef FS_STR_FILE_RUNNING
+#define FS_STR_FILE_RUNNING "File running"
+#endif
+#ifndef FS_STR_MOUNTED
+#define FS_STR_MOUNTED "FS mounted"
+#endif
 
 #ifdef __cplusplus
 }
