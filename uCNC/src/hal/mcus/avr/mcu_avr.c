4a65ce4e2accf6af5739dd3d3ab1bb837a837c88
patryk3211 <patrykmierzy@gmail.com>
2024-07-23 13:03:03 +0200

    Optimized AVR SPI polling

--- uCNC/src/hal/mcus/avr/mcu_avr.c
+++ uCNC/src/hal/mcus/avr/mcu_avr.c
@@ -1061,7 +1061,7 @@ void mcu_spi_config(uint8_t mode, uint32_t frequency)
 
 static volatile const uint8_t *spi_bulk_data_ptr_tx = 0;
 static uint8_t *spi_bulk_data_ptr_rx = 0;
-static volatile uint16_t spi_bulk_data_len = 0;
+static uint16_t spi_bulk_data_len = 0;
 
 ISR(SPI_STC_vect, ISR_NOBLOCK) {
 	// Read received byte
@@ -1091,7 +1091,7 @@ bool mcu_spi_bulk_transfer(const uint8_t *tx_data, uint8_t *rx_data, uint16_t da
 		SPDR = *spi_bulk_data_ptr_tx++;
 	}
 
-	if(spi_bulk_data_len == 0)
+	if(!(SPCR & (1 << SPIE)))
 	{
 		spi_bulk_data_ptr_tx = 0;
 		spi_bulk_data_ptr_rx = 0;
