a9261965102b2dd0e375b5d6da1b97401af758d7
Paciente8159 <Paciente8159@users.noreply.github.com>
2025-03-31 10:11:20 +0100

    Update module.h

--- uCNC/src/module.h
+++ uCNC/src/module.h
@@ -28,7 +28,7 @@ extern "C"
 #include <stdint.h>
 #include <stdbool.h>
 
-#define UCNC_MODULE_VERSION 11200
+#define UCNC_MODULE_VERSION 11201
 
 #define EVENT_CONTINUE false
 #define EVENT_HANDLED true
