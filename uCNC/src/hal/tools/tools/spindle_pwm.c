7eee9aa5e12e2a9b79b7147a822c0387b537eecd
Paciente8159 <Paciente8159@users.noreply.github.com>
2026-02-16 14:39:32 +0000

    udpated encoder tools reading

--- uCNC/src/hal/tools/tools/spindle_pwm.c
+++ uCNC/src/hal/tools/tools/spindle_pwm.c
@@ -44,7 +44,7 @@
 #endif
 #endif
 
-#ifdef SPINDLE_PWM_HAS_RPM_ENCODER
+#ifdef SPINDLE_RPM_ENCODER
 #ifndef ENABLE_ENCODER_RPM
 #error "To use RPM encoder you must enable ENABLE_ENCODER_RPM in the HAL"
 #endif
@@ -125,8 +125,8 @@ static int16_t range_speed(int16_t value, uint8_t conv)
 
 static uint16_t get_speed(void)
 {
-#ifdef SPINDLE_PWM_HAS_RPM_ENCODER
-	return encoder_get_rpm();
+#ifdef SPINDLE_RPM_ENCODER
+	return encoder_get_rpm(SPINDLE_RPM_ENCODER);
 #else
 #if ASSERT_PIN(SPINDLE_PWM)
 	return tool_get_setpoint();
