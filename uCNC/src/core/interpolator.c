e4621def76212cff100a771ddc6eef798d3d1ea9
Paciente8159 <paciente8159@gmail.com>
2026-04-27 23:28:03 +0100

    Revert "Merge pull request #960 from Paciente8159/itp-hold-recalc-bug-fix"
    This reverts commit 69526c18dac0f408f592678c3b113fec15a6ae92, reversing
    changes made to b608bc99e074fea1fcc830bfdfc832c8d8d506a3.

--- uCNC/src/core/interpolator.c
+++ uCNC/src/core/interpolator.c
@@ -473,8 +473,7 @@ void itp_run(void)
 			deaccel_from = remaining_steps;
 			itp_needs_update = true;
 		}
-		
-		if (itp_needs_update) // forces recalculation of acceleration and deacceleration profiles
+		else if (itp_needs_update) // forces recalculation of acceleration and deacceleration profiles
 		{
 			itp_needs_update = false;
 			float exit_speed_sqr = planner_get_block_exit_speed_sqr();
