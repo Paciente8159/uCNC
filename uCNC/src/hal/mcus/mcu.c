fd10ad1387d956dd5937ac34a0e8d2052958f18a
Paciente8159 <Paciente8159@users.noreply.github.com>
2026-09-08 20:47:48 +0100

    fix SPI2 pin initialization (#990)
    * fix pin initialization
    
    * added missing defaults and pin initializations
    
    ---------
    
    Co-authored-by: Paciente8159 <12116536+Paciente8159@users.noreply.github.com>

--- uCNC/src/hal/mcus/mcu.c
+++ uCNC/src/hal/mcus/mcu.c
@@ -832,7 +832,7 @@ static void FORCEINLINE mcu_coms_init(void)
 #if ASSERT_PIN_IO(SPI_SDO)
 	mcu_config_output(SPI_SDO);
 #endif
-#if ASSERT_PIN(SPI_CS)
+#if ASSERT_PIN_IO(SPI_CS)
 	mcu_config_output(SPI_CS);
 #endif
 	mcu_spi_init();
@@ -843,12 +843,15 @@ static void FORCEINLINE mcu_coms_init(void)
 	mcu_config_output(SPI2_CLK);
 #endif
 #if ASSERT_PIN_IO(SPI2_SDI)
-	mcu_config_output(SPI2_SDI);
+	mcu_config_input(SPI2_SDI);
+#ifdef SPI2_SDI_PULLUP
+	mcu_config_pullup(SPI2_SDI);
+#endif
 #endif
 #if ASSERT_PIN_IO(SPI2_SDO)
 	mcu_config_output(SPI2_SDO);
 #endif
-#if ASSERT_PIN(SPI2_CS)
+#if ASSERT_PIN_IO(SPI2_CS)
 	mcu_config_output(SPI2_CS);
 #endif
 	mcu_spi2_init();
@@ -944,6 +947,19 @@ void __attribute__((weak)) mcu_io_init(void)
 #ifndef mcu_io_reset
 void __attribute__((weak)) mcu_io_reset(void)
 {
+}
+#endif
+
+#if !defined(mcu_config_analog)
+void __attribute__((weak)) mcu_config_analog(uint8_t pin)
+{
+	(void)pin;
+}
+#endif
+#if !defined(mcu_config_input_isr)
+void __attribute__((weak)) mcu_config_input_isr(uint8_t pin)
+{
+	(void)pin;
 }
 #endif
 
